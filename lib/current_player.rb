b52f09cde9e3944d62b1adeb19cfff8a9fdf3890
William <william.l.munoz@gmail.com>
2020-04-18 23:28:02 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,7 +1,7 @@
 def turn_count(board)
   turns = 0
   board.each do |turn|
-    if turn == "O" || turn == "X"
+    if turn == "X" || turn == "O"
       turns += 1
   end
 end
@@ -10,8 +10,8 @@ end
 
 def current_player(board)
   if turn_count(board) == 1 then
-    player = "X"
-  else
     player = "O"
+  else
+    player = "X"
   end
 end
