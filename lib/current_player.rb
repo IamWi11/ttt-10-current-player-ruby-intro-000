598ff8e2ddf30ccfc2724e0467d5a812d4125b11
William <william.l.munoz@gmail.com>
2020-04-18 22:45:20 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,9 +1,8 @@
 def turn_count
   turn = 0
-  board.each do |index|
-    if index == "X" || index == "O"
-      turn +=1
-    end
+  board.each {|index|
+    if index == "X" || index == "O"}
+      turn += 1
   end
 return turn
 end
